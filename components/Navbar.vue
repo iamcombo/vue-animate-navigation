ac138d33b98ce21652de81fca8802208f5ffb5b8
Piset Heang <piset2cambo@gmail.com>
2020-07-15 01:18:18 +0700

    init

---
+++ components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navigation">
+    <ul>
+      <li><nuxt-link to="/">start</nuxt-link></li>
+      <li><nuxt-link to="/about">about</nuxt-link></li>
+      <li><nuxt-link to="/uses">uses</nuxt-link></li>
+      <li><nuxt-link to="/blog">blog</nuxt-link></li>
+      <li><nuxt-link to="/policy">policy</nuxt-link></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+  .navigation {
+    width: 100vw;
+    min-height: 100vh;
+    padding: 16px;
+    background: #0b8457;
+  }
+  ul {
+    list-style: none;
+  }
+  li {
+    margin-bottom: 16px;
+    text-transform: uppercase;
+  }
+  a {
+    color: #fefefe;
+    text-decoration: none;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="containerr" :class="{'shrink': showNav}" @click="showNav = !showNav">
+    <div>
+      <Logo />
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showNav: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.containerr {
+  width: 100vw;
+  min-height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  position: absolute;
+  top: 0;
+  background: #fff;
+  transition: .3s all ease-in-out;
+
+  border-radius: 6px;
+  cursor: pointer;
+  overflow: hidden;
+
+}
+
+.shrink {
+  transform: scale(.9) translateX(250px);
+  overflow: hidden;
+  box-shadow: -5px 5px 20px #333;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
